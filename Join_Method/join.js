8429f53457e7779e19a36c1dab604a7026ffbb54
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-13 12:30:25 +0600

    join() method are complete

---
+++ Join_Method/join.js
@@ -0,0 +1,16 @@
+// In JavaScript, the join() method is used to join all elements 
+// of an array into a single string and return the resulting string.
+
+const fruits = ['Apple', 'Banana', 'Orange'];
+
+// Join elements with default separator (comma)
+const result1 = fruits.join(); // 
+// console.log(result1);
+
+// Join elements with a custom separator (space)
+const result2 = fruits.join(' ');
+// console.log(result2);
+
+// Join elements with a custom separator (hyphen)
+const result3 = fruits.join('-');
+// console.log(result3);
